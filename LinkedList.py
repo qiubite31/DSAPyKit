6d7ca2092b9350a9b5390d47cb48bc679859a026
qiubite31 <qiubite31@gmail.com>
2018-06-05 23:37:08 +0800

    add insert / delete by index and size function

--- LinkedList.py
+++ LinkedList.py
@@ -25,46 +25,69 @@ class SinglyLinkedList(object):
             self.tail.next = self._append_item(new_item)
             self.tail = self.tail.next
 
-    def _find(self, tg_node):
+    def _find(self, tg_node, idx):
         node = self.head
         pre_node = None
-        while not tg_node == node.val:
-            pre_node = node
-            node = node.next
+        if tg_node:
+            while not tg_node == node.val:
+                pre_node = node
+                node = node.next
+            else:
+                return (pre_node, node)
         else:
-            return (pre_node, node)
+            cnt = 0
+            while node.next:
+                if cnt == idx:
+                    return (pre_node, node)
+                pre_node = node
+                node = node.next
+                cnt += 1
+            else:
+                return (pre_node, node)
 
     def insert(self, new_item=None, tg_item=None, idx=-1, loc='after'):
         if not self.head:
             return None
 
-        pre_node, node = self._find(tg_item)
+        pre_node, node = self._find(tg_item, idx)
         new_node = self._append_item(new_item)
         if loc == 'after':
             new_node.next = node.next
             node.next = new_node
         elif loc == 'before':
-            pre_node.next = new_node
-            new_node.next = node
+            if pre_node:
+                pre_node.next = new_node
+                new_node.next = node
+            else:  # 如果insert在第一筆前面要變成head
+                new_node.next = self.head
+                self.head = new_node
         else:
             return None
 
-    def delete(self, tg_item):
+    def delete(self, tg_item=None, idx=-1):
         if not self.head:
             return None
 
-        pre_node, node = self._find(tg_item)
-        pre_node.next = node.next
+        if self.size() < idx + 1 and not idx == -1:
+            return None
 
-    def _append(self, new_node):
+        pre_node, node = self._find(tg_item, idx)
+        if pre_node:
+            pre_node.next = node.next
+        else:
+            self.head = node.next
+
+    def size(self):
+        size = 1
         if not self.head:
-            self.head = self._append_item(new_node)
+            return 0
         else:
             node = self.head
             while node.next:
                 node = node.next
+                size += 1
 
-            node.next = self._append_item(new_node)
+            return size
 
     def __str__(self):
         items = []
@@ -84,5 +107,9 @@ if __name__ == '__main__':
     singlylist.append(5)
     singlylist.insert(new_item=7, tg_item=4, loc='before')
     singlylist.delete(4)
-    # singlylist.insert()
+    singlylist.insert(new_item=8, idx=0, loc="before")
+    singlylist.delete(idx=0)
+    singlylist.delete(3)
+    print(singlylist.size())
+    # singlylist.delete(idx=10)
     print(singlylist)
